24014f37ca497cea85ba4162faf23b641ac77527
Ruslan Valeev <ruvaleev@gmail.com>
2020-04-01 17:37:13 +0700

    get programs/autocomplete endpoint added

--- app/api/programs_resource.rb
+++ app/api/programs_resource.rb
@@ -7,5 +7,17 @@ class ProgramsResource < Grape::API
     get '/' do
       Program.all
     end
+
+    desc 'Autucomplete by popular programs'
+
+    params do
+      requires :term, type: String, desc: "Chunk of program's title"
+    end
+
+    get '/autocomplete' do
+      query = "title LIKE '%#{params[:term]}%'"
+      programs = Program.where(query).order(users_count: :desc).limit(10)
+      programs.presence || I18n.t('programs.not_found')
+    end
   end
 end
--- spec/api/programs_resource_spec.rb
+++ spec/api/programs_resource_spec.rb
@@ -17,4 +17,47 @@ RSpec.describe ProgramsResource, type: :request do
       expect(programs.map { |program| response.body.include?(program.to_json) }.uniq).to eq [true]
     end
   end
+
+  describe 'GET programs/autocomplete' do
+    subject(:request) { get 'programs/autocomplete', params: params }
+
+    let(:params) { { term: search_string } }
+    let(:search_string) { FFaker::Lorem.characters(10) }
+
+    context 'when programs found' do
+      let(:top_rated_program) { create(:program, title: "Best #{search_string}", users_count: 2) }
+      let(:low_rated_program) { create(:program, title: "Good #{search_string}", users_count: 1) }
+      let!(:found_programs) { [low_rated_program, top_rated_program] }
+      let!(:program_without_search_string) { create(:program) }
+
+      before { request }
+
+      it "doesn't return programs without search sting" do
+        expect(response.body).to_not include program_without_search_string.title
+      end
+      it 'returns all programs with search string sorted by :users_count' do
+        expect(JSON.parse(response.body)).to eq sorted_by_rating(found_programs).map(&:attributes)
+      end
+      it 'returns status 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+    context 'when programs not found' do
+      before { request }
+
+      it "returns 'not found' message if no found results" do
+        expect(JSON.parse(response.body)).to eq I18n.t('programs.not_found')
+      end
+
+      it 'returns status 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+end
+
+private
+
+def sorted_by_rating(programs)
+  programs.sort { |x, y| y.users_count <=> x.users_count }
 end
